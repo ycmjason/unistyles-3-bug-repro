d9ad5e22625634ef8a2bdcb8be70c8b06c20b63b
Jason Yu <me@ycmjason.com>
2025-02-14 11:59:25 +0000

    further simplify?

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,18 @@
+import { View } from 'react-native';
 import { ScopedTheme, StyleSheet } from 'react-native-unistyles';
-import { TestText } from '../components/MyText';
 
 export default function Index() {
   return (
     <ScopedTheme name="dark">
-      <TestText style={styles.x}>hi</TestText>
+      <View style={[styles.a, styles2.x]}>hi</View>
     </ScopedTheme>
   );
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
+  a: {},
+}));
+
+const styles2 = StyleSheet.create((theme, rt) => ({
   x: {},
 }));
