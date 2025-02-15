9340e95e3e2c755353f758197061f65cc980bdbd
Jason Yu <me@ycmjason.com>
2025-02-15 12:34:50 +0000

    scoped theme confused when inline style mixes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,26 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import { ScopedTheme, StyleSheet } from 'react-native-unistyles';
 
 export default function Index() {
   return (
     <ScopedTheme name="dark">
-      <View style={[styles.a, styles2.x]}>hi</View>
+      <View style={[styles.x, { justifyContent: 'center' }]}>
+        <Text style={[styles.y, { textAlign: 'center' }]}>hi</Text>
+        <Text style={[styles.y]}>hi</Text>
+      </View>
+      <View style={[styles.x]}>
+        <Text style={[styles.y, { textAlign: 'center' }]}>hi</Text>
+        <Text style={[styles.y]}>hi</Text>
+      </View>
     </ScopedTheme>
   );
 }
 
 const styles = StyleSheet.create((theme, rt) => ({
-  a: {},
-}));
-
-const styles2 = StyleSheet.create((theme, rt) => ({
-  x: {},
+  x: {
+    backgroundColor: theme.background,
+  },
+  y: {
+    color: theme.text,
+  },
 }));
