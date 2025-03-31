317f81cc7ee55a29f9532dd75bffc47da2ee644c
Jason Yu <me@ycmjason.com>
2025-03-31 16:52:56 +0100

    web image size not respected

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,54 +1,19 @@
-import { Pressable, type PressableProps, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
-
-const BaseButton = ({ style, ...props }: PressableProps) => {
-  return (
-    <Pressable
-      style={state => [baseStyles.base, typeof style === 'function' ? style(state) : style]}
-      {...props}
-    />
-  );
-};
-
-const baseStyles = StyleSheet.create(theme => ({
-  base: {
-    padding: 8,
-  },
-}));
-
-////
-
-const Button = ({ style, ...props }: PressableProps) => {
-  return (
-    <BaseButton
-      style={state => [buttonStyles.button, typeof style === 'function' ? style(state) : style]}
-      {...props}
-    />
-  );
-};
-
-const buttonStyles = StyleSheet.create(theme => ({
-  button: {
-    backgroundColor: theme.background,
-  },
-}));
-
-///
-
-const Button2 = ({ style, ...props }: PressableProps) => {
-  return (
-    <Button style={state => [typeof style === 'function' ? style(state) : style]} {...props} />
-  );
-};
-
-///
 
 export default function Index() {
   return (
     <View>
-      <Button2>
-        <Text>Working</Text>
-      </Button2>
+      <Image source={require('../assets/rick.png')} />
+      <Image width={5} height={5} source={require('../assets/rick.png')} />
+      <Image style={styles.img} source={require('../assets/rick.png')} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  img: {
+    width: 20,
+    height: 20,
+  },
+});
